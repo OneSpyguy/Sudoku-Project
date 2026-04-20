5426d4758bbfc849afab8771ea3e903040292682
charlesi <charlesi@ufl.edu>
2026-04-20 00:59:57 -0400

    Board Class

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -62,6 +62,27 @@ class Cell:
         # elif self.sketch != 0:
         #     pass
 
+class Board:
+    def __int__(self, width, height, screen, difficulty):
+
+    def draw(self):
+
+    def select(self, row, col):
+
+    def click(self, x, y):
+
+    def sketch(self, value):
+
+    def place_number(self, value):
+
+    def reset_to_original(self):
+
+    def is_full(self):
+
+    def update_board(self):
+
+    def check_board(self):
+
 ######################### SudokuGenerator Class ############################
 import math
 import random
